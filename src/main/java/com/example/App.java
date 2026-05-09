50ea2120077af85ff635b90a9327b32168c8043a
root <root@ip-172-31-18-38.us-east-2.compute.internal>
2026-05-09 18:36:55 +0000

    Initial commit with sample Java project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from MyJavaProject!");
+    }
+
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test
+    public void testAddNumbers() {
+        App app = new App();
+        assertEquals(5, app.addNumbers(2, 3));
+    }
+}
